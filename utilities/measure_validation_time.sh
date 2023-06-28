4333236fff002b2cff3dffb4f39b48951767d98d
Adam T. Geller <atgeller@cs.ubc.ca>
2023-06-28 12:44:57 -0700

    Git didn't add script

---
+++ utilities/measure_validation_time.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "name,mean_1,sem_1,mean_2,sem_2" > validation_time.csv
+
+for i in `cat utilities/annotated_benchmark_list` ;
+do
+    path=$(dirname ${i})
+    benchmark=$(basename ${i%.*})
+    >&2 echo Evaluating $benchmark
+
+    >&2 echo Validating with plain wasmtime
+    eval "hyperfine --warmup 3 --runs 10 --export-json validation_time/$(echo $benchmark)_plain.json $(printf "'~/wasm-tools/target/release/wasm-tools validate %s/%s_plain.wat'" $path $benchmark)" >> /dev/null
+
+    >&2 echo Validating with prechk wasmtime
+    eval "hyperfine --warmup 3 --runs 10 --export-json validation_time/$(echo $benchmark)_prechk.json $(printf "'~/wasm-tools/target/release/wasm-tools validate %s/%s_prechk.wat'" $path $benchmark)" >> /dev/null
+
+    python3 utilities/generate_csv_line_from_hyperfine.py $benchmark validation_time.csv --json_files validation_time/$(echo $benchmark)_plain.json validation_time/$(echo $benchmark)_prechk.json
+done
