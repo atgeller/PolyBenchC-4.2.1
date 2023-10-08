265fd86a6d1ceeb9da3cc1dba2c7294856e567aa
Adam T. Geller <atgeller@cs.ubc.ca>
2023-10-07 20:38:43 -0700

    No underscore in vmguard configuration

--- utilities/measure_run_time.sh
+++ utilities/measure_run_time.sh
@@ -24,9 +24,9 @@ do
     eval "hyperfine --warmup 3 --runs 10 --export-json run_time/$(echo $benchmark)_no_checks.json $(printf "'~/no_checks/target/release/wasmtime --allow-precompiled %s/%s_3.cwasm'" $path $benchmark)" >> /dev/null
 
     >&2 echo Compiling with vm guards
-    ~/vm_guards/target/release/wasmtime compile $path/${benchmark}_plain.wat -o $path/${benchmark}_4.cwasm
+    ~/vmguards/target/release/wasmtime compile $path/${benchmark}_plain.wat -o $path/${benchmark}_4.cwasm
     >&2 echo Running
-    eval "hyperfine --warmup 3 --runs 10 --export-json run_time/$(echo $benchmark)_vm_guards.json $(printf "'~/vm_guards/target/release/wasmtime --allow-precompiled %s/%s_4.cwasm'" $path $benchmark)" >> /dev/null
+    eval "hyperfine --warmup 3 --runs 10 --export-json run_time/$(echo $benchmark)_vmguards.json $(printf "'~/vmguards/target/release/wasmtime --allow-precompiled %s/%s_4.cwasm'" $path $benchmark)" >> /dev/null
 
-    python3 utilities/generate_csv_line_from_hyperfine.py $benchmark run_time.csv --json_files run_time/$(echo $benchmark)_plain.json run_time/$(echo $benchmark)_prechk.json run_time/$(echo $benchmark)_no_checks.json run_time/$(echo $benchmark)_vm_guards.json
+    python3 utilities/generate_csv_line_from_hyperfine.py $benchmark run_time.csv --json_files run_time/$(echo $benchmark)_plain.json run_time/$(echo $benchmark)_prechk.json run_time/$(echo $benchmark)_no_checks.json run_time/$(echo $benchmark)_vmguards.json
 done
